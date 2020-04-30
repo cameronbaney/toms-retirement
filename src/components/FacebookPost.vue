8af109b6bde8e30f281b43a570a4263c0265ad0a
Cameron Baney <cbaney@gmail.com>
2020-04-29 20:19:13 -0400

    Add netlify CMS

--- src/components/FacebookPost.vue
+++ src/components/FacebookPost.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="facebook-post">
     <!-- <g-image alt="Example image" src="~/favicon.png" width="135" /> -->
-    <p class="text-base mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quasi est tenetur animi corrupti asperiores quis ratione sit repellat enim, obcaecati aut numquam porro id illo nobis similique aspernatur deleniti.</p>
-    <p class="text-sm italic tracking-wide text-gray-700 font-medium">Megan Morris</p>
+    <p class="text-base mb-2">{{ message }}</p>
+    <p v-if="author" class="text-sm italic tracking-wide text-gray-700 font-medium">{{ author }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FacebookPost'
+  name: 'FacebookPost',
+
+  props: {
+    author: {
+      type: String,
+      default: ''
+    },
+    message: {
+      type: String,
+      default: ''
+    }
+  }
 }
 </script>
 
